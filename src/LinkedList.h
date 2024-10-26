431f76adcaac3fd37c92348e70be5a907fbaffa4
Nizan Mandelblit <nmandelblit@een.com>
2024-10-27 01:23:23 +0200

    init

---
+++ src/LinkedList.h
@@ -0,0 +1,23 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+class LinkedList {
+public:
+    LinkedList();
+    ~LinkedList();
+
+    void insert(int value);
+    bool remove(int value);
+    bool contains(int value) const;
+    int size() const;
+
+private:
+    struct Node {
+        int data;
+        Node* next;
+    };
+    Node* head;
+};
+
+#endif // LINKEDLIST_H
+
